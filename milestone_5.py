ab7d6e96ce0d975a82d2ef07e5dfcc2c45917fe5
Danilenzo <danilvorobjov@hotmail.co.uk>
2023-11-22 13:53:44 +0000

    added comments

--- milestone_5.py
+++ milestone_5.py
@@ -2,32 +2,38 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
+        # Initialize the Hangman game with a list of words and the number of lives
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word = self._choose_random_word()
+        self.word = self._choose_random_word()  # Select a random word from the list
         self.word_guessed = ['_'] * len(self.word)
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
 
-    def choose_random_word(self):
+    def _choose_random_word(self):
+        # Helper function to choose a random word from the word list
         return random.choice(self.word_list)
 
     def update_word_guessed(self, guess):
+        # Update the word_guessed list based on the correct guess
         for i, letter in enumerate(self.word):
             if letter == guess:
                 self.word_guessed[i] = guess
         self.num_letters -= 1
 
-    def handle_correct_guess(self, guess):
+    def _handle_correct_guess(self, guess):
+        # Handle a correct guess, updating the guessed word
         print(f"Good guess! '{guess}' is in the word.")
-        self._update_word_guessed(guess)
+        self.update_word_guessed(guess)
 
-    def handle_incorrect_guess(self, guess):
+    def _handle_incorrect_guess(self, guess):
+        # Handle an incorrect guess, decrement lives and provide feedback
         self.num_lives -= 1
         print(f"Sorry, '{guess}' is not in the word.")
         print(f"You have {self.num_lives} lives left.")
 
     def check_guess(self, guess):
+        # Check if the guess is correct or incorrect and take appropriate actions
         guess = guess.lower()
 
         if guess in self.word:
@@ -36,6 +42,7 @@ class Hangman:
             self._handle_incorrect_guess(guess)
 
     def ask_for_input(self):
+        # Ask the player for a guess, handle invalid inputs and check the guess
         while True:
             guess = input("Guess a letter: ")
 
@@ -44,11 +51,12 @@ class Hangman:
             elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
             else:
-                self._check_guess(guess)
+                self.check_guess(guess)
                 self.list_of_guesses.append(guess)
             break
 
     def play_game(self):
+        # Main game loop that continues until the player wins or loses
         while True:
             if self.num_lives == 0:
                 print("You lost!")
