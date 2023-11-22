98a0139585dd73379cafe3b67ae7c05c5137bddf
Danilenzo <danilvorobjov@hotmail.co.uk>
2023-11-22 13:50:28 +0000

    Added comments

--- milestone_2.py
+++ milestone_2.py
@@ -1,12 +1,16 @@
 import random
 
+# Creating list of fruits
 word_list = ['apple', 'banana', 'orange', 'tangerine', 'peach']
 
+# Randomly chooses a fruit from a list
 word = random.choice(word_list)
 
 print(word)
 
 guess = input("Enter a single alphabetical character: ")
+
+# Checks if user input is alphabetical 
 if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
 else:
