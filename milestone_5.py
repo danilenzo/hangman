6f7c78ddaa16ea9f9b0bcf94527c4572e8c01ca5
Danilenzo <danilvorobjov@hotmail.co.uk>
2023-11-22 12:43:27 +0000

    adds a final version of milestone_5.py

--- milestone_5.py
+++ milestone_5.py
@@ -4,15 +4,15 @@ class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word = self.choose_random_word()
+        self.word = self._choose_random_word()
         self.word_guessed = ['_'] * len(self.word)
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
 
-    def choose_random_word(self):
+    def _choose_random_word(self):
         return random.choice(self.word_list)
 
-    def update_word_guessed(self, guess):
+    def _update_word_guessed(self, guess):
         for i, letter in enumerate(self.word):
             if letter == guess:
                 self.word_guessed[i] = guess
@@ -20,20 +20,20 @@ class Hangman:
 
     def _handle_correct_guess(self, guess):
         print(f"Good guess! '{guess}' is in the word.")
-        self.update_word_guessed(guess)
+        self._update_word_guessed(guess)
 
-    def handle_incorrect_guess(self, guess):
+    def _handle_incorrect_guess(self, guess):
         self.num_lives -= 1
         print(f"Sorry, '{guess}' is not in the word.")
         print(f"You have {self.num_lives} lives left.")
 
-    def check_guess(self, guess):
+    def _check_guess(self, guess):
         guess = guess.lower()
 
         if guess in self.word:
-            self.handle_correct_guess(guess)
+            self._handle_correct_guess(guess)
         else:
-            self.handle_incorrect_guess(guess)
+            self._handle_incorrect_guess(guess)
 
     def ask_for_input(self):
         while True:
@@ -44,14 +44,23 @@ class Hangman:
             elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
             else:
-                self.check_guess(guess)
+                self._check_guess(guess)
                 self.list_of_guesses.append(guess)
+            break
 
-            break  # For testing purposes, you might want to remove this break
+    def play_game(self):
+        while True:
+            if self.num_lives == 0:
+                print("You lost!")
+                break
+            elif self.num_letters > 0:
+                self.ask_for_input()
+            else:
+                print("Congratulations. You won the game!")
+                break
 
-# Example usage:
-hangman_game = Hangman(['apple', 'banana', 'orange'])
-hangman_game.ask_for_input()
-print(f"Updated word_guessed: {hangman_game.word_guessed}")
-print(f"Remaining unique letters: {hangman_game.num_letters}")
-print(f"Remaining lives: {hangman_game.num_lives}")
+
+# You can add any words you like to the word_list
+word_list = ['apple', 'banana', 'orange', 'tangerine', 'peach']
+hangman_game = Hangman(word_list)
+hangman_game.play_game()
